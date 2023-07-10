3821da003e7ab32f0ee4a59750d52659cb2bc5d1
lambert34 <aleksandrlambert@gmail.com>
2023-07-10 18:05:29 +0700

    прописал обработчик событий для кнопки клианОлл(АС)

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ let finish = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['–', '+', 'X', '÷'];
+
+// дисплей
+
+const display = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    display.textContent = 0;
+    finish = false;
+    a = '';
+    b = '';
+    sing = '';
+}
+
+document.querySelector('.AC').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('AC')) return;
+}
