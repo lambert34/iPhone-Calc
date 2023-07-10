80ad289670b4e940f20b2adfda0d4b59868cbcfe
lambert34 <aleksandrlambert@gmail.com>
2023-07-10 20:05:58 +0700

    пофиксил пару ошибок

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let a = ''; // first number
 let b = ''; // second number
-let sing = ''; // operation simbol
+let sign = ''; // operation simbol
 let finish = false; 
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
 const action = ['–', '+', 'X', '÷'];
 
 // дисплей
@@ -11,16 +11,36 @@ const action = ['–', '+', 'X', '÷'];
 const display = document.querySelector('.calc-screen p');
 
 function clearAll () {
-    display.textContent = 0;
-    finish = false;
     a = '';
     b = '';
-    sing = '';
+    sign = '';
+    finish = false;
+    display.textContent = 0;
 }
 
-document.querySelector('.AC').onclick = clearAll;
+document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.buttons').onclick = (event) => {
     if(!event.target.classList.contains('btn')) return;
-    if(event.target.classList.contains('AC')) return;
+    if(event.target.classList.contains('ac')) return;
+
+    display.textContent = '';
+    // получить нажатую кнопку
+    const key = event.target.textContent;
+
+    // при нажатии кнопок с 0 по 9 или запятая
+    if(digit.includes(key)) {
+        a += key;
+        console.log(a, b , sing);
+        display.textContent = a;
+    }
+
+    // при нажатии клавиши + -, X, ÷
+    if(action.includes(key)) {
+        sing = key;
+        display.textContent = sing;
+        console.log( a, b , sing);
+        return;
+    }
 }
+
