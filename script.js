0177c90ed1644f81fe390072a975c860a85d063c
lambert34 <aleksandrlambert@gmail.com>
2023-07-10 17:54:47 +0700

    задал строки для операций и массивы значений
    внес некоторые правки в верстку и стили для большей похожести на калькулятор

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = ''; // first number
+let b = ''; // second number
+let sing = ''; // operation simbol
+let finish = false; 
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['–', '+', 'X', '÷'];
